103476863c4729c466d0b4e90f3d4bb67578b0db
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-01-09 13:45:31 -0500

    access and test api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react"
+import axios from "axios";
 
 function App() {
+
+  const getQuote = () => {
+    axios.get('https://api.quotable.io/random')
+      .then((res) => {
+        alert(res.data.content);
+      }).catch((err) => {
+        alert(err);
+      })
+    }
+  
+  useEffect(() => {
+    getQuote();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
     </div>
+    
   );
 }
 
